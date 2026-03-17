2eb4406fb16dd6cf1fa809f45f3dc62b3d56ea57
e-mandy <andymfrd02@gmail.com>
2026-03-17 15:12:12 +0000

    fix(assets): Changed the options and settings for the success component

--- src/components/Success.tsx
+++ src/components/Success.tsx
@@ -1,30 +1,21 @@
-import { useLottie, useLottieInteractivity } from 'lottie-react'
+import { useLottie } from 'lottie-react'
 import SuccessData from "../assets/lotties/Success.json";
 
 const style = {
     height: 100
 }
 
-const options = {
-    animationData: SuccessData,
-    autoplay: false
-}
 
 const Success = () => {
-    const lottieObj = useLottie(options, style);
-    const Animation = useLottieInteractivity({
-        lottieObj,
-        mode: "scroll",
-        actions: [
-            {
-                visibility: [0.4, 0.9],
-                type: "seek",
-                frames: [0, 53]
-            }
-        ]
-    });
+    const options = {
+        animationData: SuccessData,
+        loop: false,
+        autoplay: true
+    }
 
-    return Animation
+    const { View } = useLottie(options, style);
+
+    return <>{View}</>
 }
 
 export default Success
