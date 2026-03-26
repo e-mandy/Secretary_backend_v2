873240d9e94ddba858f178ce7ce885fb3c74fb5b
e-mandy <andymfrd02@gmail.com>
2026-03-26 16:46:22 +0000

    feat: initial commit

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,8 +1,22 @@
+import esgis_logo from "../assets/auth/logo-esgis.png"
+import {
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from "@/components/ui/sidebar"
 
 const SideBar = () => {
   return (
-    <div>
-      
+    <div className="h-full">
+      <div className="flex flex-col justify-start pl-10 py-3 border-b border-gray-300">
+        <img src={esgis_logo} className="md:w-30" />
+        <h1 className="text-2xl font-extrabold">Secretary Esgis</h1>
+      </div>
+
+      <div>
+        
+      </div>
     </div>
   )
 }
