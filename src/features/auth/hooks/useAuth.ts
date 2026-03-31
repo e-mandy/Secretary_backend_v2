6d363f20540740d58000cede575f17883e152b0d
e-mandy <andymfrd02@gmail.com>
2026-04-01 00:50:25 +0100

    feat(professor): Set up the professor create component

--- src/features/auth/hooks/useAuth.ts
+++
@@ -1,9 +0,0 @@
-import { useAuthStore } from "../store/auth.store"
-
-export const useAuth = () => {
-    const { user, token } = useAuthStore();
-
-    const isAuth = user != null && token != null;
-
-    return isAuth;
-}
