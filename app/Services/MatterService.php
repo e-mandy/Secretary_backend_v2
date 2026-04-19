508e199469c272fe0e5bd4823bdfd0ef0a5053c1
e-mandy <andymfrd02@gmail.com>
2026-04-19 16:30:54 +0000

    feat(matter): Set up the GET method for the matter controller

--- app/Services/MatterService.php
+++ app/Services/MatterService.php
@@ -2,10 +2,13 @@
 
 namespace App\Services;
 
+use App\Http\Resources\Matter\MatterResource;
 use App\Models\Matter;
 
 class MatterService{
     public function index(){
         $matters = Matter::select("id", "name");
+
+        return MatterResource::collection($matters);
     }
 }
