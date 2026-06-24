a4fb3bef2e7ecae83d28c319f8345a4b4d1323fd
e-mandy <andymfrd02@gmail.com>
2026-06-24 12:17:15 +0100

    feat(defense_report): Added the defense report service

--- app/Services/DefenseReportService.php
+++ app/Services/DefenseReportService.php
@@ -6,5 +6,11 @@ use App\Http\Requests\DefenseReport\StoreDefenseReportRequest;
 class DefenseReportService {
     public function create(CreateDefenseReportDTO $data, StoreDefenseReportRequest $request){
         if(!$request->hasFile($data->file)) abort(400, "Vous devez charger le pv de soutenance !!");
+
+        $file = $request->file('file');
+        $file_path = $file->store('uploads/defense_reports', 'public');
+
+
+        
     }
 }
